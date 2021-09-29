aae20103c30fb1a186fe64436272313935ef4699
Abaev <abaev@image-spark.ru>
2021-09-29 11:32:31 +0400

    Remake UserModel from model to dbmodel

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -19,10 +19,6 @@ class UserModel extends DBModel
     protected $tableName = "users";
     protected $idName = "user_id";
 
-    public function save($data)
-    {
-
-    }
     public function create($data)
     {
         $query = "
@@ -30,8 +26,8 @@ class UserModel extends DBModel
             (login, first_name, last_name, bday, active)
             VALUES(
                 \"".$data["login"]."\",
-                \"".$data["first_name"]."\",
-                \"".$data["last_name"]."\",
+                \"".$data["fname"]."\",
+                \"".$data["lname"]."\",
                 '".$data["bday"]."',
                 ".$data["active"]."
             )
@@ -42,7 +38,52 @@ class UserModel extends DBModel
        
         DBAdapter::execSQL($query);
 
-        var_dump(mysqli_error($conn));
-        var_dump($query);
+        //var_dump(mysqli_error($conn));
+        //var_dump($query);
+    }
+
+    public function update($id, $data)
+    {
+        /* // Нахер надо...
+        $changeStrings = array();
+
+        if(isset($data["login"]))
+        {
+            $changeString []= "login = \"".$data["login"]."\"";
+        }
+
+        $changeString = "";
+
+        $curLine = 1;
+        foreach($changeStrings as $line)
+        {
+            $changeString .= $line;
+            if($curLine != count($changeStrings))
+            {
+                $changeString .= ",";
+            }
+        }
+        */
+
+        $query = "
+            UPDATE ".$this->tableName."
+            SET
+            login = \"".$data["login"]."\",
+            first_name = \"".$data["fname"]."\",
+            last_name = \"".$data["lname"]."\",
+            bday = \"".$data["bday"]."\",
+            active = \"".$data["active"]."\"
+            WHERE user_id = ".$id.";
+        ";
+        DBAdapter::execSQL($query);
+    }
+
+    public function delete($id)
+    {
+        $query = "
+            DELETE FROM ".$this->tableName."
+            WHERE user_id=".$id.";
+        ";
+        DBAdapter::execSQL($query);
     }
 }
