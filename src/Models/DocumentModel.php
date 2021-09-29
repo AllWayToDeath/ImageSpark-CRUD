79f813254cb59cad6c347c0c36d7e4a5a0a8569c
Abaev <abaev@image-spark.ru>
2021-09-29 13:33:13 +0400

    Remake DocumentModel from model to dbmodel

--- src/Models/DocumentModel.php
+++ src/Models/DocumentModel.php
@@ -2,9 +2,88 @@
 
 namespace Models;
 
-use Models\Model;
+use Core\DBAdapter;
 
-class DocumentModel extends Model
+class DocumentModel extends DBModel
 {
     public const SAVEPATH = "./data/documents/";
+
+    protected $tableName = "documents";
+    protected $idName = "document_id";
+
+    public function create($data)
+    {
+        $query = "
+            INSERT INTO ".$this->tableName."
+            (
+                organisation,
+                counteragent,
+                signer,
+                dateofcontract_start,
+                dateofcontract_finish,
+                objectofcontract,
+                currency,
+                costofcontract,
+                req_address,
+                req_inn,
+                req_chacc
+            )
+            VALUES(
+                \"".$data["organisation"]."\",
+                \"".$data["counteragent"]."\",
+                \"".$data["signer"]."\",
+                '".$data["dateofcontract"]["start"]."',
+                '".$data["dateofcontract"]["finish"]."',
+                \"".$data["objectofcontract"]."\",
+                \"".$data["currency"]."\",
+                ".$data["costofcontract"].",
+                \"".$data["requisites"]["address"]."\",
+                ".$data["requisites"]["inn"].",
+                \"".$data["requisites"]["chacc"]."\"
+            )
+        ";
+       
+        DBAdapter::execSQL($query);
+
+        // $inst = DBAdapter::getInstance();
+        // $conn = $inst->getConnection();
+        // var_dump(mysqli_error($conn));
+    }
+
+    public function update($id, $data)
+    {
+        $query = "
+            UPDATE ".$this->tableName."
+            SET
+            organisation = \"".$data["organisation"]."\",
+            counteragent = \"".$data["counteragent"]."\",
+            signer = \"".$data["signer"]."\",
+            dateofcontract_start = '".$data["dateofcontract"]["start"]."',
+            dateofcontract_finish = '".$data["dateofcontract"]["finish"]."',
+            objectofcontract = \"".$data["objectofcontract"]."\",
+            currency = \"".$data["currency"]."\",
+            costofcontract = ".$data["costofcontract"].",
+            req_address = \"".$data["requisites"]["address"]."\",
+            req_inn = ".$data["requisites"]["inn"].",
+            req_chacc = \"".$data["requisites"]["chacc"]."\"
+            WHERE document_id = ".$id.";
+        ";
+
+        DBAdapter::execSQL($query);
+
+        var_dump($query);
+
+        // $inst = DBAdapter::getInstance();
+        // $conn = $inst->getConnection();
+        // var_dump(mysqli_error($conn));
+    }
+
+    public function delete($id)
+    {
+        $query = "
+            DELETE FROM ".$this->tableName."
+            WHERE document_id=".$id.";
+        ";
+        DBAdapter::execSQL($query);
+    }
 }
--- src/index.php
+++ src/index.php
@@ -14,12 +14,17 @@ require_once "vendor/autoload.php";
 
 use Models\DBModel;
 use Models\UserModel;
+use Models\DocumentModel;
 
-$model = new DBModel();
-$list = $model->list();
+/*==========|Начало отладочного кода|==========*/
+
+// $model = new DBModel();
+// $list = $model->list();
 //var_dump($list);
 
+
 $uModel = new UserModel();
+/*
 $uModel->create([
     "login" => "Car",
     "first_name" => "Lightning",
@@ -27,10 +32,74 @@ $uModel->create([
     "bday" => "2000-04-04",
     "active" => "1" 
 ]);
+*/
+/*
+$uModel->update(3, [
+    "login" => "Car",
+    "fname" => "Lightning",
+    "lname" => "MacQuin",
+    "bday" => "2006-06-06",
+    "active" => "0" 
+]);
+*/
+/*
+$uModel->create([
+    "login" => "Car",
+    "first_name" => "Lightning",
+    "last_name" => "MacQuin",
+    "bday" => "2000-04-04",
+    "active" => "0" 
+]);
+*/
+//$uModel->delete(5);
+
+$dModel = new DocumentModel();
+/*
+$dModel->create([
+    "organisation" => "DriveMegaSega",
+    "counteragent" => "Sega",
+    "dateofcontract" => array(
+        "start" => "1951-04-01",
+        "finish" => "2021-09-29"
+    ),
+    "objectofcontract" => "Game station",
+    "currency" => "₽",
+    "costofcontract" => "700",
+    "requisites" => array(
+        "address" => "Germany",
+        "inn" => "123456789012",
+        "chacc" => "CompanyGerSeGa"
+    )
+]);
+*/
+
+/*
+$dModel->update(2, [
+    "organisation" => "Nintendo Switch",
+    "counteragent" => "Nintendo",
+    "dateofcontract" => array(
+        "start" => "1900-04-01",
+        "finish" => "2021-09-29"
+    ),
+    "objectofcontract" => "Game station",
+    "currency" => "₽",
+    "costofcontract" => "800",
+    "requisites" => array(
+        "address" => "SomeWhere",
+        "inn" => "123456789012",
+        "chacc" => "CompanyNineTenDo"
+    )
+]);
+*/
+
+$dModel->delete(3);
 
 return;
+
+/*==========|Конец отладочного кода|==========*/
+
 use Core\Router;
 
-/** @var Router */
+//** @var Router */
 $router = Router::getInstance();
 $router->run();
